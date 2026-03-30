16e9df0ebd507129cf4b60726b7104549089ea2c
javierdejesusda <javier.dejesusj9@gmail.com>
2026-03-31 00:15:03 +0200

    chore: remove tracked video files, tighten docs gitignore

--- video/src/scenes/act4/BlackPause.tsx
+++
@@ -1,6 +0,0 @@
-import { AbsoluteFill } from "remotion";
-import { COLORS } from "../../lib/constants";
-
-export const BlackPause: React.FC = () => {
-  return <AbsoluteFill style={{ backgroundColor: COLORS.bg }} />;
-};
