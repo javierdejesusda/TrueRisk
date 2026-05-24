6654dfee66a5d0f69497d24bbf12f4aa13ca5165
javierdejesusda <javier.dejesusj9@gmail.com>
2026-05-25 00:45:19 +0200

    Revert "fix(frontend): inline metadata exports in twitter-image to satisfy Turbopack"
    This reverts commit 231163379542a0c3cd517d150afc3362250ce680.

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,8 +1,1 @@
-import OpenGraphImage from './opengraph-image';
-
-export const runtime = 'nodejs';
-export const alt = 'TrueRisk - Multi-Hazard Climate Risk Intelligence for Spain';
-export const size = { width: 1200, height: 630 };
-export const contentType = 'image/png';
-
-export default OpenGraphImage;
+export { default, runtime, alt, size, contentType } from './opengraph-image';
